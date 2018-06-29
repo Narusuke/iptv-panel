dbebc47e03ddad00ea91f5bfee4cdd20e145d564
Narusuke <40676766+Narusuke@users.noreply.github.com>
2018-06-28 23:38:58 -0300

    update time streams

--- views/streams.blade.php
+++ views/streams.blade.php
@@ -103,7 +103,7 @@
 										     $horas = floor($segundos/ 3600);
 	                                         $minutos = floor(($segundos - ($horas * 3600)) / 60);
 	                                         $segundos = $segundos - ($horas * 3600) - ($minutos * 60);
-	                                         echo $horas . ':' . $minutos . ":" . $segundos;									
+	                                         echo $horas . ' hora ' . $minutos . " minuto " . $segundos . " segundo";									
                                              ?>
                                             @else
                                                 Empty
